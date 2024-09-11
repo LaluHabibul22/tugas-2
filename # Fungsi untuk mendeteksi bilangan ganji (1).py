3f7274528eac004ec91b97f445af3563404a3336
LaluHabibul22 <laluhabibulmudji@icloud.com>
2024-09-12 00:50:22 +0800

    Update # Fungsi untuk mendeteksi bilangan ganji (1).py

---
+++ # Fungsi untuk mendeteksi bilangan ganji (1).py
@@ -0,0 +1,8 @@
+# Input bilangan
+n = int(input("Masukkan bilangan: "))
+
+# Cek apakah bilangan genap atau ganjil
+if n % 2 == 0:
+    print(f"{n} adalah bilangan genap.")
+else:
+    print(f"{n} adalah bilangan ganjil.")
